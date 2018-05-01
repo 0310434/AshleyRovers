b792f518bc07ac0efeb409d8b57429a6809d7be7
0310434 <0310434@rgu.ac.uk>
2018-05-01 15:29:09 +0100

    tt

--- upload/upload.php
+++ upload/upload.php
@@ -1,5 +1,5 @@
 <?php
-$target_dir = "/Applications/MAMP/htdocs/AshleyRovers/upload/";
+$target_dir = "../upload/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
