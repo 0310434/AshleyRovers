8dc4e423eb64a5e06228ddfb7e6aba90372a833d
0310434 <0310434@rgu.ac.uk>
2018-05-01 15:21:06 +0100

    tt

--- login/connection.php
+++ login/connection.php
@@ -1,16 +1,14 @@
 <?php
 
-DEFINE ('DB_USER','dbuser');
-DEFINE ('DB_PSWD','Password');
-DEFINE ('DB_HOST','localhost');
-DEFINE ('DB_NAME','records');
+DEFINE ('DB_USER','0310434');
+DEFINE ('DB_PSWD','0310434');
+DEFINE ('DB_SERVER','CSDM-WEBDEV');
+DEFINE ('DB_NAME','db0310434_players');
 
 
-$dbcon = mysqli_connect(DB_HOST,DB_USER,DB_PSWD,DB_NAME);
+$dbcon = mysqli_connect(DB_SERVER,DB_USER,DB_PSWD,DB_NAME);
 
 if (!$dbcon) {
-   die('error connecting to database');
-   }
-
-//echo "Connected successfully";
+    die('error connecting to database');
+}
 ?>
